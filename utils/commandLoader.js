a32af226b24f30e0a9c850d6fda519284ec7537c
VONDO 😺☕ <akonanik93@gmail.com>
2025-06-19 22:00:24 +0600

    Update commandLoader.js

--- utils/commandLoader.js
+++ utils/commandLoader.js
@@ -1,1 +1,16 @@
+const fs = require("fs");
+const path = require("path");
 
+let commands = {};
+
+fs.readdirSync(path.join(__dirname, "../commands")).forEach(file => {
+  if (file.endsWith(".js")) {
+    const name = file.split(".")[0];
+    commands[name] = require(`../commands/${file}`);
+  }
+});
+
+module.exports = commands;
+
+
+---
